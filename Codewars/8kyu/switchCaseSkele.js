a599dfdc1ef038003d41dd45a9590ab0fab1ad7e
Aman Saxena <58439663+amans0508@users.noreply.github.com>
2020-06-19 01:34:43 +0530

    switch skele

---
+++ Codewars/8kyu/switchCaseSkele.js
@@ -0,0 +1,15 @@
+switch(n){
+    case 1:  
+      //code block
+      break;
+    case 2:
+      //code block
+      break;
+    case ...:
+      //code block
+      break;
+  
+    default:       //default is optional, sometimes it can be omitted
+      //code block
+                   //The last one does not need break
+  }
