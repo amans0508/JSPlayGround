5766ff0d10a64d821897a3d83ca21065491f167a
Aman Saxena <58439663+amans0508@users.noreply.github.com>
2020-06-20 23:00:23 +0530

    WhileExample

--- Codewars/8kyu/switchCaseSkele.js
+++ Codewars/8kyu/switchCaseSkele.js
@@ -1,15 +1,24 @@
-switch(n){
-    case 1:  
-      //code block
+function howManydays(month) {
+  var days;
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      days = 31;
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      days = 30;
       break;
     case 2:
-      //code block
+      days = 28;
       break;
-    case ...:
-      //code block
-      break;
-  
-    default:       //default is optional, sometimes it can be omitted
-      //code block
-                   //The last one does not need break
   }
+  return days;
+}
